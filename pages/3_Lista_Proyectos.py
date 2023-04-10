738fd83976edafa49c03f6423c1e76a78c55116d
agavirja <gaviriaalejandro@hotmail.com>
2023-04-10 13:46:34 -0500

    actualziar codigo

--- pages/3_Lista_Proyectos.py
+++ pages/3_Lista_Proyectos.py
@@ -134,64 +134,65 @@ with st.container():
         col1, col2 = st.columns(2)
         with col1:
             edit_button = st.button('Editar')
-            if edit_button:
-                
-                col1, col2 = st.columns(2)
-                with col1: 
-                    nuevo_nombre_proyecto = st.text_input('Nuevo nombre del proyecto',value="")
-                    
-                with col2: 
-                    direccion   = st.text_input('Nueva dirección del proyecto',value="")
-            
-                if nuevo_nombre_proyecto!="" or direccion!="":
-                    result_button = st.button('Editar Información')
-                    if result_button:
-                        consulta = ""
-                        if direccion!="":
-                            direccion_input = copy.deepcopy(direccion)
-                            address    = formato_direccion(direccion)
-                            city       = dataproyectos[dataproyectos['Proyecto']==nombre_proyecto]['Ciudad'].iloc[0]
-                            direccion  = f'{address},{city},Colombia'
-                            response   = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address={direccion}&key=AIzaSyAgT26vVoJnpjwmkoNaDl1Aj3NezOlSpKs').json()
-                            latitud           = response['results'][0]["geometry"]["location"]['lat']
-                            longitud          = response['results'][0]["geometry"]["location"]['lng']
-                            direccion_formato = response['results'][0]["formatted_address"]
-                            
-                            db_connection   = sql.connect(user=user, password=password, host=host, database=schema)
-                            databarrio      = pd.read_sql(f"""SELECT scacodigo,scanombre as barriocatastral FROM proyect.data_barrios_colombia WHERE ST_CONTAINS(geometry, Point({longitud},{latitud})) LIMIT 1""", con=db_connection)
-                            scacodigo       = None
-                            barriocatastral = None
-                            if databarrio.empty is False and databarrio['scacodigo'].iloc[0] is not None:
-                                scacodigo   = databarrio['scacodigo'].iloc[0]
-                            if databarrio.empty is False and databarrio['barriocatastral'].iloc[0] is not None:
-                                barriocatastral = databarrio['barriocatastral'].iloc[0]
-                            consulta = f"""UPDATE `proyect`.`cbre_proyecto` SET `address` = '{direccion_input}', `latitud` = '{latitud}',  `longitud` = '{longitud}',  `scacodigo` = '{scacodigo}',  `barriocatastral` = '{barriocatastral}' ,  `direccion_formato` = '{direccion_formato}'  """
-                                
-                        if nuevo_nombre_proyecto!="":
-                            nuevo_nombre_proyecto = nuevo_nombre_proyecto.upper()
-                            if consulta=="":
-                                consulta = f"""UPDATE `proyect`.`cbre_proyecto` SET `project` = '{nuevo_nombre_proyecto}' """
-                            else:
-                                consulta = consulta + f""" ,`project` = '{nuevo_nombre_proyecto}'  """
-                        if consulta!="":
-                            consulta = consulta + f""" WHERE (`project` = '{nombre_proyecto}');"""
-                            cursor   = db_connection.cursor()
-                            cursor.execute(consulta)
-                            db_connection.commit()
-                            st.success("Editado exitosamente")
 
         with col2:
             delete_button = st.button('Eliminar proyecto')
-            if delete_button:
+
+        
+        if edit_button:
+            
+            col1, col2 = st.columns(2)
+            with col1: 
+                nuevo_nombre_proyecto = st.text_input('Nuevo nombre del proyecto',value="")
+                
+            with col2: 
+                direccion   = st.text_input('Nueva dirección del proyecto',value="")
+        
+            if nuevo_nombre_proyecto!="" or direccion!="":
+                result_button = st.button('Editar Información')
+                if result_button:
+                    consulta = ""
+                    if direccion!="":
+                        direccion_input = copy.deepcopy(direccion)
+                        address    = formato_direccion(direccion)
+                        city       = dataproyectos[dataproyectos['Proyecto']==nombre_proyecto]['Ciudad'].iloc[0]
+                        direccion  = f'{address},{city},Colombia'
+                        response   = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address={direccion}&key=AIzaSyAgT26vVoJnpjwmkoNaDl1Aj3NezOlSpKs').json()
+                        latitud           = response['results'][0]["geometry"]["location"]['lat']
+                        longitud          = response['results'][0]["geometry"]["location"]['lng']
+                        direccion_formato = response['results'][0]["formatted_address"]
+                        
+                        db_connection   = sql.connect(user=user, password=password, host=host, database=schema)
+                        databarrio      = pd.read_sql(f"""SELECT scacodigo,scanombre as barriocatastral FROM proyect.data_barrios_colombia WHERE ST_CONTAINS(geometry, Point({longitud},{latitud})) LIMIT 1""", con=db_connection)
+                        scacodigo       = None
+                        barriocatastral = None
+                        if databarrio.empty is False and databarrio['scacodigo'].iloc[0] is not None:
+                            scacodigo   = databarrio['scacodigo'].iloc[0]
+                        if databarrio.empty is False and databarrio['barriocatastral'].iloc[0] is not None:
+                            barriocatastral = databarrio['barriocatastral'].iloc[0]
+                        consulta = f"""UPDATE `proyect`.`cbre_proyecto` SET `address` = '{direccion_input}', `latitud` = '{latitud}',  `longitud` = '{longitud}',  `scacodigo` = '{scacodigo}',  `barriocatastral` = '{barriocatastral}' ,  `direccion_formato` = '{direccion_formato}'  """
+                            
+                    if nuevo_nombre_proyecto!="":
+                        nuevo_nombre_proyecto = nuevo_nombre_proyecto.upper()
+                        if consulta=="":
+                            consulta = f"""UPDATE `proyect`.`cbre_proyecto` SET `project` = '{nuevo_nombre_proyecto}' """
+                        else:
+                            consulta = consulta + f""" ,`project` = '{nuevo_nombre_proyecto}'  """
+                    if consulta!="":
+                        consulta = consulta + f""" WHERE (`project` = '{nombre_proyecto}');"""
+                        cursor   = db_connection.cursor()
+                        cursor.execute(consulta)
+                        db_connection.commit()
+                        st.success("Editado exitosamente")
+                        
+        if delete_button:
+            delete_button_2 = st.button('Seguro de eliminar proyecto?')
+            if delete_button_2:
                 db_connection = sql.connect(user=user, password=password, host=host, database=schema)
                 cursor        = db_connection.cursor()
                 cursor.execute(f"""UPDATE `proyect`.`cbre_proyecto` SET `activo` = '0'  WHERE (`project` = '{nombre_proyecto}'); """)
                 db_connection.commit()
                 st.success("Elininado exitosamente")
-        
-            
-        
-        
             
     
     
